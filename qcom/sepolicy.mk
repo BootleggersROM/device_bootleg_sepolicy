2a8e4a0fe38a12455f0dcc6e8d23d9b7119ca204
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-07-05 11:25:12 -0400

    sepolicy: update qcom/sepolicy.mk
    Change-Id: Ib30abbc335657e72b5faafefe86db4dfe6c2c53e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,5 +3,20 @@
 # that inherit from Bootleggers
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/bootleg/sepolicy/qcom/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/bootleg/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/bootleg/sepolicy/qcom/dynamic \
     device/bootleg/sepolicy/qcom/vendor
+endif
